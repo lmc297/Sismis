53602a326f11c005f8902777311e331798faa7e9
Martin Larralde <martin.larralde@embl.de>
2026-03-10 14:38:58 +0100

    Update `setup.py` to download Pfam HMMs with PyHMMER v0.12

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ class build_data(setuptools.Command):
         # Load domain whitelist from the type classifier data
         domains_file = os.path.join("sismis", "types", "domains.tsv")
         self.info(f"loading domain accesssions from {domains_file}")
-        with open(domains_file, "rb") as f:
+        with open(domains_file, "r") as f:
             domains = [line.strip() for line in f]
 
         # Download and binarize required HMMs
@@ -248,7 +248,7 @@ class build_data(setuptools.Command):
             dst = ctx.enter_context(open(output, "wb"))
             for hmm in HMMFile(src):
                 nsource += 1
-                if hmm.accession.split(b".")[0] in domains:
+                if hmm.accession.split(".")[0] in domains:
                     nwritten += 1
                     hmm.write(dst, binary=True)
         # log number of HMMs kept in final files
